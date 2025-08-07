9c3ce43587ec239536bf6b4368a7c4616d0566e5
YakomazovPavel <root@DESKTOP-VOLV4TE.localdomain>
2025-08-08 00:10:40 +0300

    change bash

--- bash_updater.sh
+++ bash_updater.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /root/projects/vaffel_tg
 COMMIT_HASH=$(git log -n 1 main --pretty=format:"%H")
 LAST_COMMIT_HASH=$(ls /tmp/git_updater/)
 if [[ "$LAST_COMMIT_HASH" =~ "$COMMIT_HASH" ]]; then
@@ -7,6 +8,7 @@ if [[ "$LAST_COMMIT_HASH" =~ "$COMMIT_HASH" ]]; then
 else
     rm -rfv /tmp/git_updater/*
     mkdir /tmp/git_updater/$COMMIT_HASH
+    git pull origin main
 fi 
 
 echo $LAST_COMMIT_HASH
