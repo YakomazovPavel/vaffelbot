63526156ece200efd371c147891ae47f11ffc5d0
YakomazovPavel <110560750+YakomazovPavel@users.noreply.github.com>
2025-08-07 18:43:29 +0000

    add database

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vaffel_tg
 backend for vaffel_tg bot
+
+
+Вход в базу данных 
+```
+docker exec -it container_name psql -U user -d vaffel
+```
--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def read_root(id: str):
 
 @app.get("/dishes/", tags=["Dishes"])
 def get_dishes():
+    # Получить инфу из бд
+    # Построить ответ в формате json
     return {"data": "Hello World"}
 
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host="0.0.0.0", port=3000, workers=1)
-
